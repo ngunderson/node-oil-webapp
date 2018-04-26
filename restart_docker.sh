aa2659b599931f5f5ccd67b9de27eb8ac2358eb5
Nathan Gunderson <gundersonn@uwplatt.edu>
2018-04-25 19:02:14 -0500

    build(docker): Initial docker setup

---
+++ restart_docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+imageName=falconer/node-oil-server:v0.0.1
+containerName=server
+
+docker build -t $imageName -f Dockerfile  .
+
+echo Delete old container...
+docker rm -f $containerName
+
+echo Run new container...
+docker run -d -p 80:8080 --name $containerName $imageName
