56886b66fe075345a9eafc89c8de2eccee4ba527
Nathan Gunderson <gundersonn@uwplatt.edu>
2018-04-30 20:25:16 -0500

    feat(realtime data): Create endpoint for retrieving most recent data
    Initially sockets were going to be leverage. However we want different
    clients to have their own updae intervals. This seemed easiest
    accomplished with setInterval on the client side.

--- public/js/realtime.js
+++ public/js/realtime.js
@@ -1,69 +1,158 @@
-// Simiple Scripts to test the dynamic elements, not final code
+//TODO: (1) Have the data from the array erase the oldest data past a number
+// of logs Ex.Circular Queue
 
+
+//var socket = new WebSocket("ws://" + location.host);
+var POLL_RATE = 5000; // 5s poll interval
+var prev_data = {};
+getLatestData();
+setInterval(getLatestData, POLL_RATE);
+
+//Setting Graph traces
+var avgTempTrace = {
+    x:[],
+    y:[],
+    type: "scatter",
+    name: "Avg Temperature"
+};
+var oilLevelTrace = {
+    x:[],
+    y:[],
+    type: "scatter",
+    name: "Oil Level"
+};
+var oilQualityTrace = {
+    x:[],
+    y:[],
+    type: "scatter",
+    name: "Oil Quality"
+};
+
+
+//Update UI components
 function updateSensors(s1,s2,s3,s4)
 {
     var sensor1 = document.getElementById("tempSensor1");
+
     var sensor2 = document.getElementById("tempSensor2");
+
     var sensor3 = document.getElementById("tempSensor3");
+
     var sensor4 = document.getElementById("tempSensor4");
-    
-    if(s1 != null)
+
+    if(s1 != undefined)
 	sensor1.innerHTML = s1 + "C";
-    if(s2 != null)
+
+    if(s2 != undefined)
 	sensor2.innerHTML = s2 + "C";
-    if(s3 != null)
+
+    if(s3 != undefined)
 	sensor3.innerHTML = s3 + "C";
-    if(s4 != null)
+
+    if(s4 != undefined)
 	sensor4.innerHTML = s4 + "C";
+
 }
 
-updateSensors(null,10,101,null);
-
+// level should be a percent
 function setOilLevel(level)
 {
     var oBar = document.getElementById("myProgress");
     var bar = document.getElementById("oilLevelBar");
     var pLabel = document.getElementById("percentLabel");
-    var amount = (level / 100) * oBar.clientHeight;
+    var amount = level * oBar.clientHeight;
     pLabel.innerHTML =  (amount / oBar.clientHeight * 100).toFixed(0) + "%";
     bar.style.height = amount;
-    
 }
-//As a percent
-setOilLevel(56);
 
-var trace1 = {
-    x: [1, 2, 3, 4], 
-    y: [10, 15, 13, 17], 
-    type: 'scatter',
-    name: 'Sensor 1'
-};
-var trace2 = {
-    x: [1, 2, 3, 4], 
-    y: [16, 5, 11, 9], 
-    type: 'scatter',
-    name: 'Sensor 2'
-};
 
+
+//Graph Update functions & STUFF
 var tempLayout = {
     title:'Temperature VS Time',
     width: 600,
     height: 250
 };
-
 var levelLayout = {
     title:'Oil Level VS Time',
     width: 600,
     height: 250
 };
-
 var qualityLayout = {
     title:'Temperature VS Time',
     width: 600,
     height: 250
 };
+function updateTempGraph()
+{
+    var data = [avgTempTrace];
+    Plotly.newPlot('oilTempGraph', data,tempLayout);
+}
+function updateLevelGraph()
+{
+    var data = [oilLevelTrace];
+    Plotly.newPlot('oilLevelGraph', data,levelLayout);
+}
+function updateQualityGraph()
+{
+    var data = [oilQualityTrace];
+    Plotly.newPlot('oilQualityGraph', data,qualityLayout);
+}
 
-var data = [trace1, trace2];
-Plotly.newPlot('oilTempGraph', data,tempLayout);
-Plotly.newPlot('oilLevelGraph', data,levelLayout);
-Plotly.newPlot('oilQualityGraph', data,qualityLayout);
+function getLatestData() {
+    console.log("Retreiving data...");
+    $.getJSON("/latestData").then(displayData);
+}
+
+function displayData(data) {
+    console.log("INFO: Message Received: ", data);
+    if (data.hasOwnProperty('default') || data.time === prev_data.time) {
+	console.log("WARNING: Server receiving no data.");
+	return;
+    }
+    if (!data.time) {
+	console.log("ERROR: No time sent by server!");
+	return;
+    }
+
+
+    //USE These JSON TAGS position is optional tags are optional//
+    //'{"temp1":"#","temp2":"#","temp3":"#","temp4":"#","avgTemp":"#","level":"#","quality":"#"}
+    var temp1 = data.temp1;
+    var temp2 = data.temp2;
+    var	temp3 = data.temp3;
+    var temp4 = data.temp4;
+    var avgTemp = data.avgTemp;
+    var oilLevel = data.level;
+    var oilQuality = data.quality;
+    var time = data.time;
+    console.log("INFO: Time is ", time);
+
+
+    //Does not matter if the values are undefined as they wont be updated
+    updateSensors(temp1,temp2,temp3,temp4);
+    //Setting OilLevel
+    if(oilLevel != undefined)
+    {
+	//var pLevel = (oilLevel / TOTALOIL) * 100
+	//or depending on what value is being sent *delete one*
+	//var pLevel = (TOTALOIL - oilLevel) / TOTALOIL * 100
+	setOilLevel(oilLevel);
+	oilLevelTrace["y"].push(oilLevel);
+	oilLevelTrace["x"].push(time);//Time Here Now time or send time over socket?
+	updateLevelGraph();
+    }
+    if(avgTemp != undefined)
+    {
+	avgTempTrace["y"].push(avgTemp);
+	avgTempTrace["x"].push(time);//Time Here Now time or send time over socket?
+	updateTempGraph();
+    }
+    if(oilQuality != undefined)
+    {
+	oilQualityTrace["y"].push(oilQuality);
+	oilQualityTrace["x"].push(time);//Time Here Now time or send time over socket?
+	updateQualityGraph();
+    }
+    prev_data = data;
+}
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const iotHubClient = require('./iot-hub.js');
+const moment = require('moment');
 
 // Constants
 const PORT = 8080;
@@ -10,17 +11,30 @@ const app = express();
 
 app.use(express.static('public'));
 
-var iotHubReader = new iotHubClient(process.env['Azure.IoT.IoTHub.ConnectionString'], process.env['Azure.IoT.IoTHub.ConsumerGroup']);
-iotHubReader.startReadMessage(function (obj, date) {
-    try {
-        console.log(date);
-        date = date || Date.now()
-        //wss.broadcast(JSON.stringify(Object.assign(obj, { time: moment.utc(date).format('YYYY:MM:DD[T]hh:mm:ss') })));
+var mostRecentMessage = {"default": "no data received"};
 
+var iotHubReader = new iotHubClient(process.env['Azure.IoT.IoTHub.ConnectionString'], process.env['Azure.IoT.IoTHub.ConsumerGroup']);
+iotHubReader.startReadMessage((obj, date) => {
+    try {
+        console.log("Message: ", obj);
+        date = date || Date.now()
+	Object.assign(obj, { time: moment.utc(date).format('hh:mm:ss')});
+	obj.temp1 = obj.temp1.toFixed(2);
+	obj.temp2 = obj.temp2.toFixed(2);
+	obj.temp3 = obj.temp3.toFixed(2);
+	obj.temp4 = obj.temp4.toFixed(2);
+	obj.avgTemp = obj.avgTemp.toFixed(2);
+	obj.level = obj.level.toFixed(2);
+	obj.quality = obj.quality.toFixed(2);
+	mostRecentMessage = obj;
     } catch (err) {
         console.log(obj);
         console.error(err);
     }
+});
+
+app.get('/latestData', (req, res) => {
+    res.send(JSON.stringify(mostRecentMessage));
 });
 
 app.listen(PORT, HOST);
