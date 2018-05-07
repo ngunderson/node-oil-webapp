8fce0cd9b1ae9476bb6606bb1597c0dfaffa5ad0
Nathan Gunderson <npgunderson@gmail.com>
2018-05-07 12:43:10 -0500

    feat(setting): Add modifying settings functionality

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,1 +1,57 @@
-console.log("settings pages");
+
+function validInput(value)
+{
+    if(value != "" && !isNaN(value))
+	if(value > 0)
+	    return true;
+    return false;
+}
+
+function isEmpty(obj)
+{
+    for(var key in obj)
+	return false;
+    return true;
+}
+
+
+function checkInputs()
+{
+    var SI = document.getElementById("radSI");
+    var IMP = document.getElementById("radIMP");
+    var units;
+    //Getting Units
+    if(SI.checked)
+	units = SI.value;
+    else
+	units = IMP.value;
+    //Collection Rate
+    var tempRate = document.getElementById("tempRate").value;
+    var levelRate = document.getElementById("levelRate").value;
+    var qualityRate = document.getElementById("qualityRate").value;
+    //& Triggers
+    var tempAlert = document.getElementById("tempAlert").value;
+    var levelAlert = document.getElementById("levelAlert").value;
+    var qualityAlert = document.getElementById("qualityAlert").value;
+
+    var obj = new Object();
+    if(validInput(tempRate))
+	obj.tempRate = tempRate;
+    if(validInput(levelRate))
+	obj.levelRate = levelRate;
+    if(validInput(qualityAlert))
+	obj.qualityRate = qualityRate;
+
+    if(validInput(tempAlert))
+	obj.T_ALERT = tempAlert;
+    if(validInput(levelAlert))
+	obj.L_ALERT = levelAlert;
+    if(validInput(qualityAlert))
+	obj.Q_ALERT = qualityAlert
+
+    if(!isEmpty(obj))
+    {
+	console.log("sending: ", obj);
+	$.post("/settings", obj);
+    }
+}
--- server.js
+++ server.js
@@ -1,22 +1,34 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const iotHubClient = require('./iot-hub.js');
 const moment = require('moment-timezone');
 const path = require('path');
 const azure = require('azure-storage');
+var Client = require('azure-iothub').Client;
+var Message = require('azure-iot-common').Message;
+var NoRetry = require('azure-iot-common').NoRetry;
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+// Alerts
+var TEMP_ALERT = 60;
+var LEVEL_ALERT = .8;
+var QUALITY_ALERT = 8;
+var TARGET_DEVICE = 'pi';
+
 // App
 const app = express();
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 storage = azure.createTableService();
 
 var mostRecentMessage = {"default": "no data received"};
 
+// streaming device to cloud messages
 var iotHubReader = new iotHubClient(process.env['Azure.IoT.IoTHub.ConnectionString'], process.env['Azure.IoT.IoTHub.ConsumerGroup']);
 iotHubReader.startReadMessage((obj, date) => {
     try {
@@ -36,6 +48,12 @@ iotHubReader.startReadMessage((obj, date) => {
         console.error(err);
     }
 });
+
+// sending cloud to device messages
+var serviceClient = Client.fromConnectionString(process.env['Azure.IoT.IoTHub.ConnectionString']);
+serviceClient.setRetryPolicy(new NoRetry());
+var connOpen = false;
+
 
 // HTML endpoints
 app.get('/', (req, res) => {
@@ -137,6 +155,50 @@ app.get('/past/quality', (req, res) => {
     }).catch((err) => {
 	console.log("QUERY ERR: ", err);
 	res.sendStatus(500);
+    });
+});
+
+app.post('/settings', (req, res) => {
+    console.log("POST: ", req.body);
+    var cmd = {};
+    cmd.T_ALERT = Number(req.body.T_ALERT);
+    cmd.L_ALERT = Number(req.body.L_ALERT);
+    cmd.Q_ALERT = Number(req.body.Q_ALERT);
+
+    if (isNaN(cmd.T_ALERT) || isNaN(cmd.L_ALERT) || isNaN(cmd.Q_ALERT) ) {
+	res.sendStatus(400);
+	return;
+    }
+
+    if (cmd.T_ALERT == TEMP_ALERT)
+	delete cmd.T_ALERT;
+    if (cmd.L_ALERT == LEVEL_ALERT)
+	delete cmd.L_ALERT;
+    if (cmd.Q_ALERT == QUALITY_ALERT)
+	delete cmd.Q_ALERT;
+
+    if (Object.keys(cmd).length == 0) {
+	res.sendStatus(200);
+	return;
+    }
+
+    serviceClient.open(function (err) {
+	if (err) {
+	    console.error('Could not connect: ' + err.message);
+	    res.sendStatus(500);
+	} else {
+	    setTimeout(function() {
+		endConnection(res);
+	    }, 2000);
+	    connOpen = true;
+	    console.log('Service client connected');
+	    serviceClient.getFeedbackReceiver(receiveFeedback);
+	    var message = new Message(JSON.stringify(cmd));
+	    message.ack = 'full';
+	    message.messageId = "My Message ID";
+	    console.log('Sending message: ' + message.getData());
+	    serviceClient.send(TARGET_DEVICE, message, handleResult);
+	}
     });
 });
 
@@ -261,4 +323,40 @@ function formatQualityData(data) {
 	x: xData,
 	y: yData
     }
+}
+
+function handleResult(err, res) {
+    if (err) console.log('send error: ' + err.toString());
+    if (res) console.log('send status: ' + res.constructor.name);
+}
+
+function receiveFeedback(err, receiver) {
+    receiver.on('message', function (msg) {
+	console.log('Feedback message:')
+	console.log(msg.getData().toString('utf-8'));
+	connOpen = false;
+
+	console.log("feedback received, closing connection...");
+	serviceClient.close(function() {
+	    console.log("feedback: connection closed");
+	});
+    });
+}
+
+function endConnection(res, cmd) {
+    console.log("timeout: closing connection");
+    if (connOpen) {
+	status = 500;
+	console.log("timeout close");
+	serviceClient.close(function() {
+	    console.log("timeout: connection closed");
+	});
+    } else {
+	TEMP_ALERT = cmd.T_ALERT || TEMP_ALERT;
+	LEVEL_ALERT = cmd.L_ALERT || LEVEL_ALERT;
+	QUALITY_ALERT = cmd.Q_ALERT || QUALITY_ALERT
+	console.log("VARS: ", TEMP_ALERT, LEVEL_ALERT, QUALITY_ALERT);
+	status = 200;
+    }
+    res.sendStatus(status);
 }
